71c5a35394ad40bcf35bbe92011663a68ab07934
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-12 19:27:02 +0200

    Setup a simple sserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use rocket::response::content::RawHtml;
+
+#[macro_use]
+extern crate rocket;
+
+type Messages = HashMap<String, Vec<String>>;
+
+#[get("/")]
+fn index() -> RawHtml<&'static str> {
+    RawHtml("<h1>Welcome to my Rocket server!</h1>")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
